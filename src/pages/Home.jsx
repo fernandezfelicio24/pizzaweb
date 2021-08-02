fbc7092af3eec0fa08cce2d1091ddaf21638a6bd
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2021-08-02 14:39:49 +0700

    Making Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+import BannerImage from '../assets/pizza.jpeg'
+import '../styles/Home.css'
 
 function Home() {
     return (
-        <div>
-            Hello World
+        <div className="home">
+            <div className="headerContainer" style={{backgroundImage: `url(${BannerImage})`}}>
+                <h1>Felicio's Pizzeria</h1>
+                <p>PIZZA KONA IMI NIA GOSTU HOTU</p>
+
+                <Link to="/menu">
+                <button> ORDER NOW </button>
+                </Link>
+                
+            </div>
         </div>
     )
 }
